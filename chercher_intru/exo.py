fb42a40671ce4fc4306fc28a209523deaf548155
Christophe Schulz <christopheschulz@orange.fr>
2025-02-12 08:49:22 +0100

    chercher intrus terminé

---
+++ chercher_intru/exo.py
@@ -0,0 +1,42 @@
+import sys
+
+def chercher_intru(array):
+    dictionnaire = {}
+    for i in range(len(array)):
+        # on vérifie si le dictionnaire existe
+        if dictionnaire.get(array[i], 0) == 0:
+            # sinon on le crée
+            dictionnaire[array[i]] = 1
+        else: 
+            # si il existe on incrémente
+            dictionnaire[array[i]] += 1
+    
+    for keys,value in dictionnaire.items():
+        if value == 1:
+            return keys
+
+    erreur()
+
+    
+
+
+def verification_arguments(arguments):
+    ok = False  
+    if len(arguments) > 2:
+        ok = True
+    return ok
+
+
+def afficher(chaine):
+    pass
+
+def erreur():
+    print("error")
+
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    if verification_arguments(arguments):
+        print(chercher_intru(arguments))
+    else:
+        erreur()
