f0553b8871b69afe351b76c1e7bd9ad6ea322d3d
Christophe Schulz <christopheschulz@orange.fr>
2025-02-12 13:43:20 +0100

    rotation vers la gauche

---
+++ rotation_vers_la_gauche/exo.py
@@ -0,0 +1,39 @@
+import sys
+
+
+def ma_rotation(array):
+    result =[]
+    for i in range(1,len(array)):
+        result.append(array[i])
+    result.append(array[0])   
+    return result
+
+
+def verification_arguments(arguments):
+    ok = False
+    if len(arguments) > 1:
+        ok = True
+    return ok
+
+
+def afficher(chaine):
+    if len(chaine) == 1:
+        print(chaine[0])
+    else:
+        for i in range(len(chaine)-1):
+            print(chaine[i],end=", ")
+        print(chaine[-1])
+        print()
+
+
+def erreur():
+    print("error")
+
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    if verification_arguments(arguments):
+        resultat = ma_rotation(arguments)
+        afficher(resultat)
+    else:
+        erreur()
