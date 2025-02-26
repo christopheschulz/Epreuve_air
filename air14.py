edf7aed92efb89a51c6d72ff76333c22bfc5d773
christophe schulz <christophe.schulz@laposte.net>
2025-02-26 10:43:28 +0100

    suite remise en forme air

---
+++ air14.py
@@ -0,0 +1,3 @@
+# air : ok
+
+print("J'ai terminé l'Épreuve de l'Air' et c'était amusant, parfois stressant mais jouissif")
--- air_ok/exo.py
+++
@@ -1,1 +0,0 @@
-print("J'ai terminé l'Épreuve de l'air' et c'était du challenge")
